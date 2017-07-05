f31ce4e4f1da5aa575d7e3ae2ff6aef669bd9911
Justin Gross <jmbg144@gmail.com>
2017-07-05 14:13:19 -0400

    Initial commit-finish through step 4

---
+++ src/main/java/com/example/demo/CustomsecurityApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomsecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomsecurityApplication.class, args);
+	}
+}
